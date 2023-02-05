b19949dd9bc21a250f9f1d4c62fadc2a85f24fd6
Henkdeploy <henkfduplooy@gmail.com>
2023-02-05 19:14:16 +1100

    add comments

--- main.py
+++ main.py
@@ -10,19 +10,27 @@ app = typer.Typer()
 @app.command()
 def vector_images(folder_path:str)-> None:
     print(f"Converting images in folder : {folder_path} to vectors")
+    #initialize resnet50 model
     model = tf.keras.applications.resnet50.ResNet50()
     iv = ImageVectors(model)
+    #Create the vectore files
     s = iv.initialize_vectors(folder_path)
 
 @app.command()
 def compare_image(query_file_path:str,image_db_path :str, n : int = 5)-> None:
     print(f"Check  {query_file_path}")
+    #initialize resnet50 model
     model = tf.keras.applications.resnet50.ResNet50()
     iv = ImageVectors(model)
+    #create cevtor file for query image
     query_image = iv.initialize_vectors(query_file_path)
+    #Get an array of the comaprison vectore files
     comp_arr = iv.get_comp_vectors(image_db_path)
+    #Get the cosine per vectore
     cosine_arr = iv.cosine_similarity(query_image,comp_arr)
+    #adding the query image at the top
     cosine_arr.append([query_file_path,100])
+    #sort to get the top similarities
     cosine_arr.sort(reverse = True,key = lambda i: i[1])
     iv.most_similar(cosine_arr,n)
 
--- src/.ipynb_checkpoints/vectorize_images-checkpoint.py
+++ src/.ipynb_checkpoints/vectorize_images-checkpoint.py
@@ -19,6 +19,7 @@ class ImageVectors:
         ImageVectors.model = arg
         
     def pre_process(self,file_path) -> np.ndarray:
+        #Do all the preproccessing needed for geting the feature array
         image = tf.keras.utils.load_img(file_path, target_size=(224, 224))
         ppx = tf.keras.utils.img_to_array(image)
         ppx = np.expand_dims(ppx, axis=0)
@@ -27,24 +28,30 @@ class ImageVectors:
     
     def convert_to_vector(self,file_path:str,file_name):
         x = self.pre_process(file_path)
+        #get the feature vectore from the preprocessed arrays
         predictions = self.model.predict(x)
         flattened_features = predictions.flatten()
+        #save array in a pickle
         np.save(f"images/tmp/{file_name.replace('.','_')}", flattened_features, allow_pickle=True, fix_imports=True)
         
     def initialize_vectors(self,folder_path:str) -> str:
         res = []
+        #vectorize a file 
         if os.path.isfile(folder_path):
             res.append(folder_path)
             self.convert_to_vector(f"{folder_path}",os.path.basename(folder_path))
             return folder_path
-        else:
+        else: #vectorize an entire folder 
+            #Count how many files in folder for progress calc
             files = len(fnmatch.filter(os.listdir(folder_path), '*.*'))
             for count , path in enumerate(os.listdir(folder_path)):
+                #Show progress cal so poeple know its not haging
                 print(f"Percentage done : {(count/files)*100}%")
                 if os.path.isfile(os.path.join(folder_path, path)):
-                    res.append(path)
+                    #Append the file path if its a proper image filke
                     try:
                         self.convert_to_vector(f"{folder_path}{path}",path)
+                        res.append(path)
                     except:
                         print(f"{os.path.join(folder_path, path)} is not valid")
             return res[0]
@@ -52,13 +59,17 @@ class ImageVectors:
     
     def get_comp_vectors(self,image_db_path:str) -> np.ndarray:
         res = []
+        #Count how many files in folder for progress calc
         files = len(fnmatch.filter(os.listdir(image_db_path), '*.*'))
         for count, path in enumerate(os.listdir(image_db_path)):
+            #Show progress cal so poeple know its not haging
             print(f"Percentage done : {(count/files)*100}%")
             if os.path.isfile(os.path.join(image_db_path, path)):
+                #Check if image has a vectorized file
                 if os.path.isfile(f"images/tmp/{path.replace('.','_')}.npy"):
                     res.append([f"images/tmp/{path.replace('.','_')}.npy",os.path.join(image_db_path, path)])
                 else:
+                    #if not then vectorize it if possible
                     print("NO FILE TRY TO VECTORIZE")
                     try:
                         self.convert_to_vector(f"{image_db_path}{path}",path)
@@ -68,6 +79,7 @@ class ImageVectors:
         return res
     
     def cosine_func(self,query_vector :np.ndarray,comp_vector :np.ndarray) -> float:
+        #Calculate the cosine difference
         cosine = np.dot(query_vector,comp_vector)/(norm(query_vector)*norm(comp_vector))
         return cosine
         
@@ -77,6 +89,7 @@ class ImageVectors:
         query_vector = f"images/tmp/{os.path.basename(query_image_file).replace('.','_')}.npy"
         cosine_arr = []
         query_vector = np.load(query_vector)
+        #calculate cosine per vector
         for comp_vec_path in image_vector_files:
             comp_vector = np.load(comp_vec_path[0])
             cosine = self.cosine_func(query_vector,comp_vector)
@@ -87,6 +100,7 @@ class ImageVectors:
             
     def most_similar(self,cosine_arr:np.ndarray,n:int):
         for count, i in enumerate(cosine_arr):
+          #  Plots a n by 6 image collage of top n picks
             plt.subplot(int(round(math.ceil(n/5),0)),6,count+1)
             img = cv2.imread(i[0])
             img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
